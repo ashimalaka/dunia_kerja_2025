ae9330b8dc68e4b6dc96f2b119e0863e71665e93
ashimalaka <mutasimbilahql@gmail.com>
2026-05-23 10:33:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,3 +35,10 @@ if middle.button("Emoji button", icon="😃", width="stretch"):
     middle.markdown("You clicked the emoji button.")
 if right.button("Material button", icon=":material/mood:", width="stretch"):
     right.markdown("You clicked the Material button.")
+import streamlit as st
+
+with st.container(horizontal=True, horizontal_alignment="distribute"):
+    "`A`" if st.button("A", shortcut="A") else "` `"
+    "`S`" if st.button("S", shortcut="Ctrl+S") else "` `"
+    "`D`" if st.button("D", shortcut="Cmd+Shift+D") else "` `"
+    "`F`" if st.button("F", shortcut="Mod+Alt+Shift+F") else "` `"
