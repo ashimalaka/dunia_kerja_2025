65935b24f1627d5a1bed8fccd751ac5ee9d07bd4
ashimalaka <mutasimbilahql@gmail.com>
2026-05-23 10:31:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,3 +16,22 @@ import streamlit as st
 
 title = st.text_input("Movie title", "Life of Brian")
 st.write("The current movie title is", title)
+import streamlit as st
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
+    import streamlit as st
+
+left, middle, right = st.columns(3)
+if left.button("Plain button", width="stretch"):
+    left.markdown("You clicked the plain button.")
+if middle.button("Emoji button", icon="😃", width="stretch"):
+    middle.markdown("You clicked the emoji button.")
+if right.button("Material button", icon=":material/mood:", width="stretch"):
+    right.markdown("You clicked the Material button.")
